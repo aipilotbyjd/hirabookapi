5551757610afcdce518c6077c5bc62555e210f1e
Jaydeep Dhrangiya <recentpagetoday@gmail.com>
2025-03-06 00:38:05 +0530

    sa

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:8000', 'http://localhost:8081', 'http://localhost'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
