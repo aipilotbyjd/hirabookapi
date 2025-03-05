d242110fe398e21211c7b2ff002442934f9bcf80
Jaydeep Dhrangiya <recentpagetoday@gmail.com>
2025-03-06 00:21:37 +0530

    sasa

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test', function () {
+Route::post('/test', function () {
     return response()->json([
         'message' => 'test',
     ]);
