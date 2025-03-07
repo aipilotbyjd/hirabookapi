c62ad23166c2b800f95143499e585d28679f8449
Jaydeep Dhrangiya <recentpagetoday@gmail.com>
2025-03-08 01:02:48 +0530

    sa

--- app/Http/Controllers/Api/v1/AuthController.php
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -420,6 +420,7 @@ class AuthController extends BaseController
                 'email' => $user->email,
                 'phone' => $user->phone,
                 'profile_image' => $user->profile_image,
+                'address' => $user->address,
             ];
 
             return $this->sendResponse($output, 'Profile fetched successfully');
